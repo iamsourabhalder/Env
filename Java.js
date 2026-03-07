a8242f36750f894673b6ee746e872a9b612d4e83
Sourab Halder <sourab.halder.in@gmail.com>
2026-03-07 21:05:55 +0530

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect, useState }
+
+from
+
+// setting the api link
+
+https://ww.omdbapi.com/?i=tt3896198&apikey=$
+
+export const API_URL =
+
+{process.env.REACT_APP_MOVIE_KEY};
+
+
+
+const AppContext = React.createContext();
